7c541ed6b988bcf60a159a602da0f633dae5feb0
Dimitri <dimitri.miranda@gmail.com>
2019-08-28 19:33:31 -0300

    add method ticker_ripple to currency xrp

--- mercadobitcoin/api.py
+++ mercadobitcoin/api.py
@@ -12,7 +12,6 @@ class Base(object):
 
         param action: The requested API action
         """
-        print("https://%s/api/%s/")
         response = requests.get("https://%s/api/%s/" % (self.host, action))
         return response.json()
 
