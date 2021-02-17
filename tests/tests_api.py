b23a807234b96de05252ceba0c6dc7cc7fed4c8f
Helder Geovane Gomes de Lima <he7d3r@gmail.com>
2021-02-17 18:10:25 -0300

    Validate coin and method and add more tests (#27)

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -18,6 +18,7 @@ def assert_orderbook_response(response):
     assert len(response['bids'][0]) == 2
 
 def assert_trades_response(response):
+    assert len(response) > 0
     trade_fields = {"date", "price", "amount", "tid", "type"}
     for field in trade_fields:
         assert field in response[0]
@@ -61,18 +62,50 @@ class ApiTestCase(unittest.TestCase):
         assert_trades_response(response)
 
     @tests.vcr.use_cassette
+    def test_trades_chainlink_from_date_to_date(self):
+        min_date = 1612545400
+        max_date = 1612545424
+        response = self.api.trades(coin='LINK', from_date=min_date,
+                                   to_date=max_date)
+        assert_trades_response(response)
+        assert len(response) > 1
+        assert response[0]['date'] == min_date
+        assert response[-1]['date'] == max_date
+        for trade in response[1:-1]:
+            assert trade['date'] > min_date
+            assert trade['date'] < max_date
+
+    @tests.vcr.use_cassette
+    def test_trades_bitcoin_cash_tid(self):
+        tid_threshold = 1616116
+        response = self.api.trades(coin='BCH', tid=tid_threshold)
+        assert_trades_response(response)
+        assert response[0]['tid'] == tid_threshold + 1
+        for trade in response:
+            assert trade['tid'] > tid_threshold
+
+    @tests.vcr.use_cassette
+    def test_trades_bitcoin_cash_since(self):
+        tid_threshold = 1616116
+        response = self.api.trades(coin='BCH', since=tid_threshold)
+        assert_trades_response(response)
+        assert response[0]['tid'] == tid_threshold + 1
+        for trade in response:
+            assert trade['tid'] > tid_threshold
+
+    @tests.vcr.use_cassette
     def test_trades_litecoin(self):
         response = self.api.trades_litecoin()
         assert_trades_response(response)
 
     @tests.vcr.use_cassette
     def test_day_summary(self):
-        response = self.api.day_summary(2013, 6, 12)
+        response = self.api.day_summary(year=2013, month=6, day=12)
         assert_summary_fields(response)
 
     @tests.vcr.use_cassette
     def test_day_summary_litecoin(self):
-        response = self.api.day_summary(2013, 8, 23, coin='LTC')
+        response = self.api.day_summary(year=2013, month=8, day=23, coin='LTC')
         assert_summary_fields(response)
 
     @tests.vcr.use_cassette
